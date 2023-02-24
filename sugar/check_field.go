21817e3bfd80a75e175e64a8944fb3cc7883353a
lianqingfeng <lianqingfeng@xiaoyezi.com>
2023-02-24 16:45:15 +0800

    init

---
+++ sugar/check_field.go
@@ -0,0 +1,93 @@
+package sugar
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+	"unicode/utf8"
+)
+
+// CheckStructField 检测结构体字段 ,检查通过err=nil
+//required :必填 |  email:邮箱  |  mobile:手机号  |  maxLen:字符串最大长度(包含)  |  minLen:字符串最小长度(包含)
+func CheckStructField(data interface{}) (err error) {
+	rv := reflect.ValueOf(data)
+	if rv.Kind() == reflect.Ptr {
+		rv = rv.Elem()
+	}
+	numField := rv.NumField()
+	for i := 0; i < numField; i++ {
+		field := rv.Type().Field(i)
+		tagName := getTagName(field.Tag.Get("check"))
+		tagFieldName := field.Tag.Get("json")
+		if tagFieldName == "" {
+			tagFieldName = field.Name
+		}
+		tagNameSlice := strings.Split(tagName, ";")
+		for _, v := range tagNameSlice {
+			tmpSlice := strings.Split(v, ":")
+			if len(tmpSlice) == 2 {
+				switch tmpSlice[0] {
+				case "maxLen":
+					lenNum, lenErr := strconv.ParseInt(tmpSlice[1], 10, 0)
+					if lenErr != nil {
+						err = errors.New(fmt.Sprintf("%s is type err, err msg:%+v", tmpSlice[0], lenErr.Error()))
+						return
+					}
+					if utf8.RuneCountInString(rv.Field(i).String()) > int(lenNum) {
+						err = errors.New(fmt.Sprintf("%s is maxLen  greater than %+v", tmpSlice[0], tmpSlice[1]))
+						return
+					}
+				case "minLen":
+					lenNum, lenErr := strconv.ParseInt(tmpSlice[1], 10, 0)
+					if lenErr != nil {
+						err = errors.New(fmt.Sprintf("%s is type err err msg:%+v", tmpSlice[0], lenErr.Error()))
+						return
+					}
+					//fmt.Println("bbbbbbbbb", utf8.RuneCountInString(rv.Field(i).String()), int(lenNum))
+					if utf8.RuneCountInString(rv.Field(i).String()) < int(lenNum) {
+						err = errors.New(fmt.Sprintf("%s is maxLen  less than %+v", tmpSlice[0], tmpSlice[1]))
+						return
+					}
+				default:
+					err = errors.New(fmt.Sprintf("%s not found tag", tmpSlice[0]))
+					return
+				}
+
+			} else if len(tmpSlice) == 1 {
+				switch v {
+				case "":
+					//这是一个特殊的判断， strings.Split 返回空字符串
+				case "required": //必填
+					if rv.Field(i).IsZero() {
+						err = errors.New(fmt.Sprintf("%s is required", tagFieldName))
+						return
+					}
+				case "email":
+					if !VerifyEmailFormat(rv.Field(i).String()) {
+						err = errors.New(fmt.Sprintf("%s is invalid eamil", tagName))
+						return
+					}
+				case "mobile":
+					if !VerifyMobileFormat(rv.Field(i).String()) {
+						err = errors.New(fmt.Sprintf("%s is invalid mobile", tagName))
+						return
+					}
+				default:
+					err = errors.New(fmt.Sprintf("field check type not found, current type %+v", v))
+					return
+				}
+			} else {
+				err = errors.New(fmt.Sprintf("%s is format err", tagName))
+				return
+			}
+		}
+	}
+	return
+}
+
+func getTagName(tagNameStr string) string {
+	tagName := strings.Split(tagNameStr, ",")
+	return tagName[0]
+}
---
+++ sugar/check_field_test.go
@@ -0,0 +1,27 @@
+package sugar
+
+import (
+	"testing"
+)
+
+func TestCheckStructField(t *testing.T) {
+	type param struct {
+		Name    string `json:"name" check:"required;maxLen:2000;minLen:7"`
+		Address string `json:"address" check:"required;email"`
+		Mobile  string `json:"mobile" check:"required;mobile"`
+	}
+
+	a := param{
+		Name:    "我已超a出度",
+		Address: "qingfeng.lian@163.com",
+		Mobile:  "18000000000",
+	}
+	err := CheckStructField(a)
+	if err == nil {
+		t.Errorf("\n最终检测结果应该有错误的\n")
+		return
+	}
+	t.Logf("这里检查结果应该是err ,err:%+v", err)
+
+	t.Logf("success")
+}
