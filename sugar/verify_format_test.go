c9c5a59439a97ce22730842febd8b7de1cdcf399
lianqingfeng <lianqingfeng@xiaoyezi.com>
2023-02-24 16:46:11 +0800

    init

---
+++ sugar/verify_format_test.go
@@ -0,0 +1,55 @@
+package sugar
+
+import "testing"
+
+func TestVerifyEmailFormat(t *testing.T) {
+	a := ""
+	aVerify := VerifyEmailFormat(a)
+	if aVerify == true {
+		t.Errorf("a check %+v", aVerify)
+		t.Fail()
+		return
+	}
+
+	b := "qingfeng.lian@test.com"
+	bVerify := VerifyEmailFormat(b)
+	if bVerify == false {
+		t.Errorf("b check %+v", bVerify)
+		t.Fail()
+		return
+	}
+
+	c := "qingfeng.--lian@test.com"
+	cVerify := VerifyEmailFormat(c)
+	if cVerify == true {
+		t.Errorf("c check %+v", cVerify)
+		t.Fail()
+		return
+	}
+}
+
+func TestVerifyMobileFormat(t *testing.T) {
+	a := ""
+	aVerify := VerifyMobileFormat(a)
+	if aVerify == true {
+		t.Errorf("a check %+v", aVerify)
+		t.Fail()
+		return
+	}
+
+	b := "17600000000"
+	bVerify := VerifyMobileFormat(b)
+	if bVerify == false {
+		t.Errorf("b check %+v", bVerify)
+		t.Fail()
+		return
+	}
+
+	c := "qingfeng.--lian@test.com"
+	cVerify := VerifyMobileFormat(c)
+	if cVerify == true {
+		t.Errorf("c check %+v", cVerify)
+		t.Fail()
+		return
+	}
+}
