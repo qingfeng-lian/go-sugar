99bc0a79692ba02a67ceab13240a3dbb8a42e8c5
清风 <qingfeng@qingfengdeMacBook-Pro.local>
2023-03-05 20:19:17 +0800

    fix get request params diss

--- sugar/http.go
+++ sugar/http.go
@@ -13,17 +13,23 @@ import (
 )
 
 // HttpGet get请求 responseData必须是指针类型
-func HttpGet(urlStr string, requestData map[string]string, responseData interface{}) error {
-	formData := url.Values{}
-	for k, v := range requestData {
-		formData.Add(k, v)
-	}
-	data := formData.Encode()
-	req, err := http.NewRequest("GET", urlStr, strings.NewReader(data))
+func HttpGet(urlStr string, requestData map[string]string, responseData interface{}, headerData map[string]string) error {
+	req, err := http.NewRequest("GET", urlStr, nil)
 	if err != nil {
 		fmt.Printf("%s", err.Error())
 		return err
 	}
+	if len(headerData) > 0 {
+		for k, v := range headerData {
+			req.Header.Set(k, v)
+		}
+	}
+	// 设置参数
+	params := req.URL.Query()
+	for k, v := range requestData {
+		params.Add(k, v)
+	}
+	req.URL.RawQuery = params.Encode()
 	client := &http.Client{
 		Timeout: 30 * time.Second,
 	}
@@ -39,7 +45,7 @@ func HttpGet(urlStr string, requestData map[string]string, responseData interfac
 		return err
 	}
 	if resp.StatusCode != http.StatusOK {
-		return errors.New("response StatusCode is not StatusOK")
+		return errors.New(fmt.Sprintf("response StatusCode is not StatusOK; code:%+v", resp.StatusCode))
 	}
 
 	fmt.Print("\n======http.Get======\n", string(respData))
@@ -52,7 +58,7 @@ func HttpGet(urlStr string, requestData map[string]string, responseData interfac
 }
 
 // HttpPost post请求 responseData必须是指针类型
-func HttpPostForm(urlStr string, requestData map[string]string, responseData interface{}) error {
+func HttpPostForm(urlStr string, requestData map[string]string, responseData interface{}, headerData map[string]string) error {
 	formData := url.Values{}
 	for k, v := range requestData {
 		formData.Set(k, v)
@@ -64,7 +70,11 @@ func HttpPostForm(urlStr string, requestData map[string]string, responseData int
 		return err
 	}
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
+	if len(headerData) > 0 {
+		for k, v := range headerData {
+			req.Header.Set(k, v)
+		}
+	}
 	client := &http.Client{
 		Timeout: 30 * time.Second,
 	}
@@ -80,7 +90,7 @@ func HttpPostForm(urlStr string, requestData map[string]string, responseData int
 		return err
 	}
 	if resp.StatusCode != http.StatusOK {
-		return errors.New("response StatusCode is not StatusOK")
+		return errors.New(fmt.Sprintf("response StatusCode is not StatusOK; code:%+v", resp.StatusCode))
 	}
 
 	fmt.Print("\n======http.Post======\n", string(respData))
@@ -93,15 +103,19 @@ func HttpPostForm(urlStr string, requestData map[string]string, responseData int
 }
 
 // HttpPost post请求 header json responseData必须是指针类型
-func HttpPost(urlStr string, requestData interface{}, responseData interface{}) error {
+func HttpPost(urlStr string, requestData interface{}, responseData interface{}, headerData map[string]string) error {
 	data, _ := json.Marshal(requestData)
 	req, err := http.NewRequest("POST", urlStr, bytes.NewReader(data))
 	if err != nil {
 		fmt.Printf("%s", err.Error())
 		return err
 	}
-	req.Header.Set("Content-Type", "application/json")
-
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	if len(headerData) > 0 {
+		for k, v := range headerData {
+			req.Header.Set(k, v)
+		}
+	}
 	client := &http.Client{
 		Timeout: 30 * time.Second,
 	}
@@ -117,7 +131,7 @@ func HttpPost(urlStr string, requestData interface{}, responseData interface{}) 
 		return err
 	}
 	if resp.StatusCode != http.StatusOK {
-		return errors.New("response StatusCode is not StatusOK")
+		return errors.New(fmt.Sprintf("response StatusCode is not StatusOK; code:%+v", resp.StatusCode))
 	}
 
 	fmt.Print("\n======http.Post======\n", string(respData))
--- sugar/http_test.go
+++ sugar/http_test.go
@@ -1,6 +1,9 @@
 package sugar
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHttpPost(t *testing.T) {
 	url := "http://op-pre.xiaoyezi.com/ref/student/identity"
@@ -15,6 +18,24 @@ func TestHttpPost(t *testing.T) {
 	}
 	req := map[string]interface{}{}
 	// err := HttpPostForm(url, res, req)
-	err := HttpPost(url, res, req)
+	err := HttpPost(url, res, req, map[string]string{})
 	t.Log("dddddddddd", err, req)
+}
+
+func TestHttpGet(t *testing.T) {
+	url := "http://dss-pre.xiaoyezi.com/internal/student"
+	type reqData struct {
+		Code int `json:"code"`
+		Data struct {
+			Id   uint64 `json:"id"`
+			Name string `json:"name"`
+		} `json:"data"`
+	}
+	res := map[string]string{
+		"id": "10211",
+	}
+	req := reqData{}
+	err := HttpGet(url, res, &req, map[string]string{})
+	t.Log(err)
+	t.Log(fmt.Sprintf("req:%+v", req))
 }
