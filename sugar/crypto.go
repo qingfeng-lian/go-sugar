03f3292800d87f96a63a84f2420543af47ef7dd9
qingfeng.lian <lianyuhang_feng@163.com>
2023-03-19 12:31:04 +0800

    add rc4 crypto

--- sugar/crypto.go
+++ sugar/crypto.go
@@ -2,9 +2,12 @@ package sugar
 
 import (
 	"crypto/md5"
+	"crypto/rc4"
+	"encoding/base64"
 	"fmt"
 )
 
+// md5 加密
 func Md5(str string) string {
 	has := md5.New()       // 创建md5算法
 	has.Write([]byte(str)) // 写入需要加密的数据
@@ -14,4 +17,43 @@ func Md5(str string) string {
 	// fmt.Println(hex.EncodeToString(b)) // 通过hex包的EncodeToString函数，将数据转为16进制字符串； 打印 e99a18c428cb38d5f260853678922e03
 	// 还有一种方法转换为16进制,通过fmt的格式化打印方法， %x表示转换为16进制
 	return fmt.Sprintf("%x", b) // 打印 e99a18c428cb38d5f260853678922e03
+}
+
+// rc4 加密
+// 当然解密同样可用这个方法
+// 为了方便区分加密解密，建议只用此方法进行加密， 解密可以用：RC4Decrypt
+func RC4Encrypt(key, str string) []byte {
+	strByte := []byte(str)
+	cipher, err := rc4.NewCipher([]byte(key))
+	if err != nil {
+		return nil
+	}
+	dst := make([]byte, len(strByte))
+	cipher.XORKeyStream(dst, strByte)
+	return dst
+}
+
+// rc4 解密
+// 当然加密同样可用这个方法
+// 为了方便区分加密解密、 建议只用此方法进行解密、建议加密用： RC4Encrypt
+func RC4Decrypt(key, str string) []byte {
+	return RC4Encrypt(key, str)
+}
+
+// rc4 加密
+// 并且返回base64 encode 后的字符串
+func RC4EncryptBaseEncode(key, str string) string {
+	strByte := RC4Encrypt(key, str)
+	return base64.StdEncoding.EncodeToString(strByte)
+}
+
+// rc4 解密
+// str 是base64 encode 的字符串
+func RC4EncryptBaseDecode(key, str string) string {
+	src, err := base64.StdEncoding.DecodeString(str)
+	if err != nil {
+		return ""
+	}
+	strByte := RC4Decrypt(key, string(src))
+	return string(strByte)
 }
--- sugar/crypto_test.go
+++ sugar/crypto_test.go
@@ -1,6 +1,9 @@
 package sugar
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestMd5(t *testing.T) {
 	qf := "qingfeng"
@@ -11,4 +14,42 @@ func TestMd5(t *testing.T) {
 		return
 	}
 	t.Log("create md5", realCreate)
+}
+
+func TestRC4Encrypt(t *testing.T) {
+	qf := "qingfeng"
+	key := "qf-test"
+	needMd5 := []byte{164, 168, 13, 224, 204, 81, 187, 96}
+
+	realCreate := RC4Encrypt(key, qf)
+	fmt.Println(realCreate)
+	if string(realCreate) != string(needMd5) {
+		t.Errorf("create rc4 error: create:%s, need:%s", realCreate, needMd5)
+		return
+	}
+	t.Log("create rc4", realCreate)
+}
+
+func TestRC4EncryptBaseEncode(t *testing.T) {
+	qf := "qingfeng"
+	key := "qf-test"
+	needMd5 := "pKgN4MxRu2A="
+	realCreate := RC4EncryptBaseEncode(key, qf)
+	if realCreate != needMd5 {
+		t.Errorf("create rc4 error: create:%s, need:%s", realCreate, needMd5)
+		return
+	}
+	t.Log("create rc4", realCreate)
+}
+
+func TestRC4EncryptBaseDecode(t *testing.T) {
+	qf := "pKgN4MxRu2A="
+	key := "qf-test"
+	needMd5 := "qingfeng"
+	realCreate := RC4EncryptBaseDecode(key, qf)
+	if realCreate != needMd5 {
+		t.Errorf("deconde rc4 error: create:%s, need:%s", realCreate, needMd5)
+		return
+	}
+	t.Log("deconde rc4", realCreate)
 }
